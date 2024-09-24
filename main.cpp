dfd8141e628410a9671de8d8c1b330422156c656
Ferchus-Casas12 <ferchus120634@outlook.com>
2024-09-24 13:14:57 -0500

    userInput

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include<iomanip>
+#include "userInputFunctions.h"
+using namespace std;
+
+int main(){
+
+    int number = getNumberInput(0, 100, "Please enter a number: ");
+
+    return 0;
+}
