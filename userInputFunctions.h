418ca340ddba11a85d36cd32bcd347203de50f81
Ferchus-Casas12 <ferchus120634@outlook.com>
2024-09-30 19:01:42 -0600

    userINPUT

--- userInputFunctions.h
+++ userInputFunctions.h
@@ -7,10 +7,9 @@ using namespace std;
 
 // these functions are for safe user Input, they prevent the program from crashing
 
-
-
 // pass parameters min and max during function call, and the message has to match the logic of these
 // function enforces user to enter a number into the console
+
 int getNumberInput(int min, int max, string message){
 
     cout << message;
