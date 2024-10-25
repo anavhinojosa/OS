69dce0a18fec7e37d3d3bacab9538dfd38d462ae
Ferchus-Casas12 <ferchus120634@outlook.com>
2024-10-25 10:59:28 -0500

    again

--- main.cpp
+++ main.cpp
@@ -4,98 +4,24 @@
 #include <iostream>
 #include "booting.h"
 #include "auth_page.h"
-// #include "process.h"
 #include "processManager.h"
 #include "consoleCommands.h"
 #include "CPUinstructions.h"
 using namespace std;
 
-<<<<<<< Updated upstream
-
-
-int main() {
-    int schedulingType;
-    cout << "Enter 1 for FIFO, 2 for SJF: ";
-    cin >> schedulingType;
-
-    return 0;
-}
-
-
-=======
 int main()
 {
     std::cout << std::unitbuf;
     clearConsole();
     bootSystem();
-    initializeQueues(); 
-//initializeQueues() starts me a ready queue with 10 processes in incresing order cycles
-            // std::queue<Process> readyQueue;
-            // std::queue<Process> waitingQueue;
->>>>>>> Stashed changes
-
+    initializeQueues();
+    // initializeQueues() starts me a ready queue with 10 processes in incresing order cycles
+    //  std::queue<Process> readyQueue;
+    //  std::queue<Process> waitingQueue;
 
     CPU cpu;
     cpu.schedulePolicy = 2; // for Round Robin
     cpu.executeProcess();
-
-<<<<<<< Updated upstream
-        switch (choice) {
-            case 1: {
-                int processChoice;
-                do {
-                    cout << "\nProcess Menu" << endl;
-                    cout << "1. Start Process" << endl;
-                    cout << "2. List Processes" << endl;
-                    cout << "3. Terminate Process" << endl;
-                    cout << "4. Back to Main Menu" << endl;
-                    cout << "Enter your choice: ";
-                    cin >> processChoice;
-
-                    switch (processChoice) {
-                        case 1:
-                            startProcess();
-                            break;
-                        case 2:
-                            listProcesses();
-                            break;
-                        case 3: {
-                            int pid;
-                            cout << "Enter PID to terminate: ";
-                            cin >> pid;
-                            terminateProcess(pid);
-                            break;
-                        }
-                        case 4:
-                            cout << "Returning to Main Menu" << endl;
-                            break;
-                        default:
-                            cout << "Invalid choice, please try again." << endl;
-                    }
-                } while (processChoice != 4);
-                break;
-            }
-            case 2: {
-                if (authenticate()) {
-                    cout << "Authentication successful!" << endl;
-                    cout << "Hello! Welcome";
-                    break;
-                } else {
-                    cout << "Failed. Try again." << endl;
-                }
-                break;
-            }
-            case 3:
-                cout << "Exiting OS Simulator" << endl;
-                break;
-
-            default:
-                cout << "Invalid choice, please try again." << endl;
-        }
-    } while (choice != 3);
-     */
-=======
-    return 0;
 }
 
 /*
@@ -170,4 +96,3 @@ int main()
 // startProcess();
 
 // return 0;
->>>>>>> Stashed changes
