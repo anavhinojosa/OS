62cd3b3ccaedf58ef7367c4ac54ac78f56b094ee
anavhinojosa <anavhinjosa0@gmail.com>
2024-11-22 16:27:40 -0600

    booting

--- Booting.h
+++ Booting.h
@@ -1,20 +1,22 @@
 #include <iostream>
+#include "Delays.h"
 
 using namespace std;
 
-void delay(int seconds) {
-  for (int i = 0; i < seconds * 100000000; ++i) {
-    //simulate the delay.
-  }
-}
-
 void bootSystem() {
-  cout << "Booting OS Simulator" << endl;
-  delay(4);
-
-  cout << "Loading..." << endl;
-  cout << "Please do not quit." << endl;
-  delay(4);
-
-  cout << "System booted" << endl;
+    cout << "[System Booting...]" << endl;
+    delay(2);
+    cout << "[OK] Powering on hardware." << endl;
+    delay(2);
+    cout << "[OK] Loading kernel modules." << endl;
+    delay(2);
+    cout << "[OK] Initializing memory management unit (MMU)." << endl;
+    delay(2);
+    cout << "[OK] Configuring interrupt handling." << endl;
+    delay(2);
+    cout << "[OK] Boot sequence complete." << endl;
+    delay(2);
+    cout << "Welcome to the OS Simulator!" << endl;
+    delay(2);
+    cout << "\n";
 }
